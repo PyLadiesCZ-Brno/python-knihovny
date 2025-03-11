bf29660a8b179cac45e0116db1cf7f2d068df43b
Jimy-Bacon <JimyBacon@gmail.com>
2025-03-11 21:16:10 +0100

    help comment
    help comment for git

--- 01/promitani.py
+++ 01/promitani.py
@@ -1,5 +1,27 @@
 # https://kodim.cz/czechitas/uvod-do-progr-2/bonusy/cykly-2/list-comprehension/promitani
 
+#git prikazy
+
+'''
+git clone https://github.com/Jimy-42/python-knihovny.git  spust ve složce kam to chces => vytvoří kopii mainu
+git switch -c ukol-test vytvori novy doplň ....... s nazvem ukol-test
+
+git status
+git add složka\soubor
+git diff
+git log
+git restore složka\soubor
+
+git push
+git pull
+git commit
+
+git push origin soubor
+
+git restore --staged složka\soubor
+
+'''
+
 def prevod(vstup):
     #jjj
     ...
