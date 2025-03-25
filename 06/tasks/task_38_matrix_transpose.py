6b2b6052ba786dc2025e129faffa5f6634c81992
Jana D <drazkova.j@gmail.com>
2025-03-25 22:53:30 +0100

    38 Transpozice matice

--- 06/tasks/task_38_matrix_transpose.py
+++ 06/tasks/task_38_matrix_transpose.py
@@ -4,4 +4,7 @@ def matrix_transpose(matrix: list[list[int]]) -> list[list[int]]:
     """
     Vrátí transponovanou matici.
     """
-
+    if matrix == []:
+        return []
+    else:
+        return [[radek[j] for radek in matrix] for j in range(len(matrix[0]))]
