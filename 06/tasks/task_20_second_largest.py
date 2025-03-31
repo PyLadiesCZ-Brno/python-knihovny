831f0d1c254ff8d417f95df67364313f2c373cef
Jimmy-42 <j.studeny42@gmail.com>
2025-04-01 00:46:33 +0200

    20 druhe nejvetsi

--- 06/tasks/task_20_second_largest.py
+++ 06/tasks/task_20_second_largest.py
@@ -4,4 +4,10 @@ def second_largest(lst: list[int]) -> int:
     """
     Vrátí druhé největší číslo v seznamu.
     """
+    lst = sorted(lst, reverse=True)
+    for finder in lst:
+        if finder < lst[0]:
+            numero = finder
+            break
 
+    return numero
